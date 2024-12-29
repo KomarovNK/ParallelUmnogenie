db57f34eb4603b8db596a4b1c3fd31d80605b684
Rakhmatulluin Anvar <anvarmulleer@gmail.com>
2024-12-29 21:09:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MPI;
+
+class PingPong
+{
+    static void Main(string[] args)
+    {
+        MPI.Environment.Run(ref args, comm =>
+        {
+            if (comm.Rank == 0)
+            {
+                Console.WriteLine("Rank 0 is alive and running on " + MPI.Environment.ProcessorName);
+                for (int dest = 1; dest < comm.Size; ++dest)
+                {
+                    Console.Write("Pinging process with rank " + dest + "...");
+                    comm.Send("Ping!", dest, 0);
+                    string destHostname = comm.Receive<string>(dest, 1);
+                    Console.WriteLine(" Pong!");
+                    Console.WriteLine("  Rank " + dest + " is alive and running on " + destHostname);
+                }
+            }
+            else
+            {
+                comm.Receive<string>(0, 0);
+                comm.Send(MPI.Environment.ProcessorName, 0, 1);
+            }
+        });
+    }
+}
